0a4c842e496f2df299bbb225b5a70a692e1153a9
OhJintaek <sunshot@hanyang.ac.kr>
2023-01-26 17:46:45 +0900

    feat: sum 기능 다르게 구현 및 scanner는 사용 후 닫아주는 것을 학습

--- src/Sum.java
+++ src/Sum.java
@@ -4,11 +4,10 @@ public class Sum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("두 수를 입력하세요.");
-        String userInput = scanner.nextLine();
-        String[] splitInput = userInput.split(" ");
-        int number1 = Integer.parseInt(splitInput[0]);
-        int number2 = Integer.parseInt(splitInput[1]);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
 
         System.out.printf("두수의 합은 %d입니다.", number1+number2);
+        scanner.close();
     }
 }
