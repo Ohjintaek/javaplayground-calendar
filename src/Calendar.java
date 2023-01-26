601f9176d123b5e0ae49917f7a9664b3c3b1102c
OhJintaek <sunshot@hanyang.ac.kr>
2023-01-26 18:00:53 +0900

    refactor: 함수를 분리하여 작성하는 것으로 개선

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Calendar {
+    private static final int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+    public int getMaxDaysOfMonth(int month) {
+        return maxDays[month - 1];
+    }
     public static void main(String[] args) {
         String tmp = "일 월 화 수 목 금 토\n" +
                 "--------------------\n" +
@@ -20,11 +25,11 @@ public class Calendar {
         System.out.println("22 23 24 25 26 27 28");*/
 
         // 월을 입력하면 그 달이 몇일로 구성되어 있는지 출력하는 프로그램 작성하기
-        int[] month = {31,28,31,30,31,30,31,31,30,31,30,31};
         Scanner scanner = new Scanner(System.in);
+        Calendar cal = new Calendar();
         System.out.println("달을 입력하세요.");
         int number = scanner.nextInt();
 
-        System.out.printf("%d월은 %d일까지 있습니다.", number, month[number-1]);
+        System.out.printf("%d월은 %d일까지 있습니다.", number, cal.getMaxDaysOfMonth(number));
     }
 }
