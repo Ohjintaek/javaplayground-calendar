751874b35ca2283029a9fa88d596ddcf4c1dc1b7
OhJintaek <sunshot@hanyang.ac.kr>
2023-01-26 17:16:00 +0900

    feat: 출력할 문장 copy&paste하여 구현

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,10 +1,19 @@
 public class Calendar {
     public static void main(String[] args) {
-        System.out.println(" 일 월 화 수 목 금 토");
+        String tmp = "일 월 화 수 목 금 토\n" +
+                "--------------------\n" +
+                " 1  2  3  4  5  6  7\n" +
+                " 8  9 10 11 12 13 14\n" +
+                "15 16 17 18 19 20 21\n" +
+                "22 23 24 25 26 27 28";
+
+        System.out.println(tmp);
+
+        /*System.out.println(" 일 월 화 수 목 금 토");
         System.out.println(" ------------------");
         System.out.println(" 1  2  3  4  5  6  7");
         System.out.println(" 8  9 10 11 12 13 14");
         System.out.println("15 16 17 18 19 20 21");
-        System.out.println("22 23 24 25 26 27 28");
+        System.out.println("22 23 24 25 26 27 28");*/
     }
 }
